2715cc876c4a4bfb8ee3422062bf621a2f6493a5
veronica20 <veronica.voskanyan@gmail.com>
2017-04-07 00:54:16 +0400

    second commit

--- main/footer.php
+++ main/footer.php
@@ -6,12 +6,12 @@
         input_value = (val.split(" ")).length == 1 ? val : val.split(" ");
 
         if (typeof input_value == 'string') {
-            if(Number(input_value)){
+            if (Number(input_value)) {
                 definitive.push(Number(input_value));
             }
         } else if (typeof input_value == 'object') {
             input_value.forEach(function (element) {
-                if(Number(element)){
+                if (Number(element)) {
                     definitive.push(Number(element));
                 }
             });
@@ -25,12 +25,32 @@
 
         var min = Math.min.apply(null, definitive),
             max = Math.max.apply(null, definitive);
-            result = definitive.indexOf(max - min);
-            if( result == -1){
-                alert('Oops : false');
-            }else{
-                alert('Congrats : true');
+        result = definitive.indexOf(max - min);
+        if (result == -1) {
+            alert('Oops : false');
+        } else {
+            alert('Congrats : true');
+        }
+        location.reload();
+    }
+
+    function getSecondMax() {
+
+        var max = definitive[0];
+        var sec = definitive[0];
+
+        for(var i = 1; i < definitive.length ; i++){
+            if(definitive[i] > max){
+                sec = max;
+                max = definitive[i];
             }
+            if(definitive[i] > sec && definitive[i] < max){
+                sec = definitive[i];
+                index_sec = i;
+
+            }
+        }
+        alert(index_sec);
         location.reload();
     }
 </script>
--- three/index.php
+++ three/index.php
@@ -6,11 +6,11 @@
     </div>
     <div class="col-md-6 ">
         <button class="btn btn-primary" onclick="addInArray( document.getElementById('input').value) "> Add to array</button>
-        <button class="btn btn-primary" onclick="myFunction()"> GO!</button>
+        <button class="btn btn-primary" onclick="getSecondMax()"> GO!</button>
     </div>
 </div>
 <div class="row mt-4">
-    <div class="col-md-3 "> Your array is:</div>
+    <div class="col-md-3 "> Your array is::</div>
     <div class="col-md-9" id="finish"></div>
 </div>
 
